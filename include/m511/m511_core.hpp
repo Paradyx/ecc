78b645f2398fed14525d556d6d41c0cdec4ad34b
Yoann Kehler <privat@yoann.de>
2017-04-04 00:44:35 +0900

    First draft of a scalar multiplication on elliptic curve

--- include/m511/m511_core.hpp
+++ include/m511/m511_core.hpp
@@ -1,13 +1,10 @@
 #ifndef M511_HPP_
 #define M511_HPP_
 
-class M511 {
-public:
-    void encrypt();
-    void decrypt();
-    void sign();
-    void verify();
-    void generate_key();
-};
+#include "m511/finitefieldelement.hpp"
+
+static FiniteFieldElement m511_add(FiniteFieldElement a, FiniteFieldElement b);
+static FiniteFieldElement m511_double(FiniteFieldElement a);
+static FiniteFieldElement m511_scalarmult(FiniteFieldElement n, FiniteFieldElement p);
 
 #endif //M511_HPP_
--- src/m511/m511_core.cpp
+++ src/m511/m511_core.cpp
@@ -1,1 +1,36 @@
 #include "m511/m511_core.hpp"
+#include "m511/finitefieldelement.hpp"
+
+static FiniteFieldElement m511_add(FiniteFieldElement a, FiniteFieldElement b){
+
+}
+
+static FiniteFieldElement m511_double(FiniteFieldElement x1, FiniteFieldElement z1, FiniteFieldElement x3, FiniteFieldElement z3){
+  //X3 = (X1+Z1)^2*(X1-Z1)^2
+  //Z3 = (4*X1*Z1)*((X1-Z1)2+a24*(4*X1*Z1))
+  FiniteFieldElement x1pz1;
+  FiniteFieldElement x1mz1;
+  FiniteFieldElement x1z14;
+  FiniteFieldElement a24;
+  a24 = 132610;
+  x1pz1 = x1+z1;
+  x1mz1 = x1-z1;
+  x1z14 = x1*z1;
+  x1z14 *= 4;
+  x3 = 1;
+  x3 *= x1pz1;
+  x3 *= x1pz1;
+  x3 *= x1mz1;
+  x3 *= x1mz1;
+  a24 *= x1z14;
+  z3 = 1;
+  z3 *= x1mz1;
+  z3 *= x1mz1;
+  z3 += a24;
+  z3 *= x1z14;
+
+}
+
+static FiniteFieldElement m511_scalarmult(FiniteFieldElement n, FiniteFieldElement p){
+
+}
