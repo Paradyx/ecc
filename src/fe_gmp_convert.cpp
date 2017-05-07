5da29b9e6d439f91bab986e57b4620138cd1326d
Yoann Kehler <privat@yoann.de>
2017-05-07 18:09:07 +0900

    Implemented export with gmp

--- src/fe_gmp_convert.cpp
+++ src/fe_gmp_convert.cpp
@@ -1,15 +1,24 @@
 #if FE_CONST==0
 
 #include "fe_gmp.hpp"
+#include "assert.h"
 
 void Fe::frombytes(const unsigned char in[64]) {
   mpz_import(this->element, 64, 1, sizeof(unsigned char), 1, 0, in);
 }
 
 void Fe::tobytes(unsigned char out[64]) {
+  unsigned char* tmp = NULL;
   size_t countp;
-  mpz_export(out, &countp, 1, sizeof(unsigned char),1, 0, this->element);
-  //TODO: Continue from here. Read gmp documentation and export correctly
+  mpz_export(tmp, &countp, 1, sizeof(unsigned char),1, 0, this->element);
+  assert(countp<=64);
+  for (int i = 0; i<countp; i++) out[i] = tmp[i];
+  for (int i = countp; i<64; i++) out[i] = 0;
+
+  // Free memory
+  void (*freefunc) (void *, size_t);
+  mp_get_memory_functions (NULL, NULL, &freefunc);
+  freefunc(tmp, countp);
 }
 
 void Fe::fromint(const int in) {
