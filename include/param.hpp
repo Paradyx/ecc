199bba936162e13dd1ce47855ad8171a0cb0ca8b
Yoann Kehler <privat@yoann.de>
2017-05-20 14:55:12 +0900

    Added Curve parameters as compiletime constants

--- include/param.hpp
+++ include/param.hpp
@@ -1,20 +1,16 @@
 #ifndef PARAM_HPP_
 #define PARAM_HPP_
 
-
-class Key_EC(){
-
-}
-
-SecByteBlock key(AES::DEFAULT_KEYLENGTH), iv(AES::BLOCKSIZE);
-string k,v;
-
-OS_GenerateRandomBlock(true, key, key.size());
-OS_GenerateRandomBlock(false, iv, iv.size());
-
-struct EC_CURVE_PARAM {
-
-    int g_nNumber;
-} TNum;
+static struct M511_PARAM {
+  unsigned char g[64] = {0x05 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 };
+  unsigned char l[64] = {0x1b, 0xaf, 0x6b, 0xb7, 0xba, 0x15, 0x4c, 0xa5,
+                         0xbf, 0x10, 0xaa, 0xa6, 0x42, 0x50, 0x12, 0xac,
+                         0xa2, 0x79, 0x37, 0xd1, 0xeb, 0xae, 0xb2, 0x7a,
+                         0x67, 0xf5, 0xc7, 0x30, 0xff, 0xfe, 0xb5, 0x17,
+                         0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0,
+                         0, 0, 0, 0, 0, 0, 0, 0x10};
+} M511;
 
 #endif //PARAM_HPP_
