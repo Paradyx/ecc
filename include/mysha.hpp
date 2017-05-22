a2892b7ff37204f5e0c017b3aacf8fbcefa0ea33
Yoann Kehler <privat@yoann.de>
2017-05-22 17:25:16 +0900

    Moved sha to a seperate file

---
+++ include/mysha.hpp
@@ -0,0 +1,6 @@
+#ifndef MYSHA_HPP
+#define MYSHA_HPP
+
+static void mysha512(unsigned char message[64], unsigned char digest[64]);
+
+#endif //MYSHA_HPP
--- src/messages/sign.cpp
+++ src/messages/sign.cpp
@@ -1,39 +1,10 @@
 #include "messages.hpp"
 #include <string>
 #include <iostream>
-#include <openssl/evp.h>
 #include "param.hpp"
 #include "keys.hpp"
 #include "scalar.hpp"
 #include "messages.hpp"
-
-static void mysha512(unsigned char message[64], unsigned char digest[64]){
-  EVP_MD_CTX *mdctx;
-  const EVP_MD *md;
-  unsigned int md_len, i;
-
-  OpenSSL_add_all_digests();
-  md = EVP_get_digestbyname("SHA512");
-
-  if(!md) {
-        printf("Unknown message digest\n");
-        exit(1);
-  }
-
-  mdctx = EVP_MD_CTX_create();
-  EVP_DigestInit_ex(mdctx, md, NULL);
-  EVP_DigestUpdate(mdctx, message, 64);
-  EVP_DigestFinal_ex(mdctx, digest, &md_len);
-  EVP_MD_CTX_destroy(mdctx);
-
-  printf("Digest is: ");
-  for(i = 0; i < md_len; i++)
-        printf("%02x", digest[i]);
-  printf("\n");
-
-  /* Call this once before exit. */
-  EVP_cleanup();
-}
 
 Signature RawBlock::sign(Private_Key &sk){
   // ECDSA Schoolbook
